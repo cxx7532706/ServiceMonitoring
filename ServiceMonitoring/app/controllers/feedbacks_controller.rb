a274b60e9beae48d6799615055d4fa920ab18f3f
jlus <jlus@users.noreply.github.com>
2015-09-17 09:39:51 +1000

    Pushing changes to feedback controller again

--- ServiceMonitoring/app/controllers/feedbacks_controller.rb
+++ ServiceMonitoring/app/controllers/feedbacks_controller.rb
@@ -1,7 +1,9 @@
 class FeedbacksController < InheritedResources::Base
 
   def new
+  	#Create a new feedback
   	@feedback = Feedback.new
+  	#Get the questions for the feedback
   	@survey = Survey.find(params[:survey])
   	@questions = @survey.questions
   end
