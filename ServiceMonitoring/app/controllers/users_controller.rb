f43d8efc14ed90f7e0b3a7c6241c9c9ae18780eb
MingyouFang <estellafang@gmail.com>
2015-09-17 16:43:55 +1000

    Add password confirmation for user registration.

--- ServiceMonitoring/app/controllers/users_controller.rb
+++ ServiceMonitoring/app/controllers/users_controller.rb
@@ -43,6 +43,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:email, :password, :is_admin)
+      params.require(:user).permit(:email, :password, :password_confirmation, :is_admin)
     end
 end
