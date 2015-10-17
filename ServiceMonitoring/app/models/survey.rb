14bb6c2f48b55499c0da774bf170851af31d6fb5
Erwin Fernandez <erwinfernandez92@gmail.com>
2015-10-17 20:22:59 +1100

    really fixed destroy survey

--- ServiceMonitoring/app/models/survey.rb
+++ ServiceMonitoring/app/models/survey.rb
@@ -1,4 +1,5 @@
 class Survey < ActiveRecord::Base
 	has_many :questions, :dependent => :destroy
+	has_many :feedbacks, :dependent => :destroy
 	accepts_nested_attributes_for :questions, :reject_if => lambda { |a| a[:title].blank? }, :allow_destroy => true
 end
